4fdcfec273bc19325e3e08dd67e27cadfd911823
Chayangkoon Tona <chayangkoon@amanothai.co.th>
2025-05-21 15:27:05 +0700

    update config path and deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://amanothai.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-public-page/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
